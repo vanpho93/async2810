9a654fd328198ed7676a4c56d277f402123b7979
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-30 17:44:58 +0700

    xong

--- b.js
+++ b.js
@@ -1,6 +1,7 @@
 const request = require('request');
 
 function cong(a, b, cb) {
+    if(isNaN(a) || isNaN(b)) return cb(new Error('Invalid input'));
     const URL = `http://localhost:3000/tinh/CONG/${a}/${b}`;
     request(URL, (err, res, body) => {
         if (err) return cb(err, null);
@@ -9,6 +10,7 @@ function cong(a, b, cb) {
 }
 
 function nhan(a, b, cb) {
+    if(isNaN(a) || isNaN(b)) return cb(new Error('Invalid input'));
     const URL = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
     request(URL, (err, res, body) => {
         if (err) return cb(err, null);
@@ -17,6 +19,7 @@ function nhan(a, b, cb) {
 }
 
 function chia(a, b, cb) {
+    if(isNaN(a) || isNaN(b)) return cb(new Error('Invalid input'));
     if (b === 0) return cb(new Error('Divide by zero'));
     const URL = `http://localhost:3000/tinh/CHIA/${a}/${b}`;
     request(URL, (err, res, body) => {
