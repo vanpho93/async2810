dd6dae432000fb5b03f969759477abc24009c69e
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-30 18:31:38 +0700

    3 function

--- c.js
+++ c.js
@@ -11,6 +11,29 @@ function cong(a, b) {
     });
 }
 
+function nhan(a, b) {
+    return new Promise((resolve, reject) => {
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Invalid input'));
+        const URL = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
+        request(URL, (err, res, body) => {
+            if (err) return reject(err);
+            resolve(body);
+        });
+    });
+}
+
+function chia(a, b) {
+    return new Promise((resolve, reject) => {
+        if (b == 0) return reject(new Error('Divide by zero'));
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Invalid input'));
+        const URL = `http://localhost:3000/tinh/CHIA/${a}/${b}`;
+        request(URL, (err, res, body) => {
+            if (err) return reject(err);
+            resolve(body);
+        });
+    });
+}
+
 cong(1, 2)
 .then(result => console.log(result))
 .catch(err => console.log(err));
